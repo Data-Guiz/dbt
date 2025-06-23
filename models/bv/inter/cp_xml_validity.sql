9bc9c6ecc76552e37a4e9a0c8be9d6cc356b60f9
Guillaume ROBIN <guillaume.robin@talan.com>
2025-06-23 21:42:58 +0200

    rm_incre

--- models/bv/inter/cp_xml_validity.sql
+++ models/bv/inter/cp_xml_validity.sql
@@ -1,29 +1,8 @@
-{{ config(
-    materialized='incremental',
-    unique_key=['booking_hk', 'file_name'],
-    schema='bv',
-    alias='cp_xml_validity'
-) }}
-
-with source as (
-    select
-        booking_hk,
-        file_name,
-        startdate,
-        enddate
-    from {{ ref('cp_xml_validity_vw') }}
-)
-
-select
-    booking_hk,
-    file_name,
-    startdate,
-    enddate
-from source
-
-{% if is_incremental() %}
-    -- Only insert new or updated records
-    where (booking_hk, file_name, enddate) not in (
-        select booking_hk, file_name, enddate from {{ this }}
+{{
+    config(
+        materialized='table'
     )
-{% endif %}
+}}
+
+Select *
+From {{ source('bv', 'cp_xml_validity') }}
